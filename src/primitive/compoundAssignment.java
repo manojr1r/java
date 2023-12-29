609f191750f7aa2776554b492243a71453e3f005
manojleolappy <manojr1r@gmail.com>
2023-12-29 10:34:56 +0530

    test

--- src/primitive/compoundAssignment.java
+++ src/primitive/compoundAssignment.java
@@ -4,6 +4,6 @@ public class compoundAssignment {
     public static void main(String[] args) {
         int result = 10;
         result-=7;
-        System.out.print(result);
+        System.out.print( result );
     }
 }
