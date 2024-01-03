38191705d1ad4bb5a56e78a903bb07bdfdbbac28
manoj <manojabeta@gmail.com>
2024-01-03 15:11:39 +0530

    tokenCheck

--- src/MethodsPractice/TeenNumberChecker.java
+++ src/MethodsPractice/TeenNumberChecker.java
@@ -2,7 +2,7 @@ package MethodsPractice;
 
 public class TeenNumberChecker {
     public static void main(String[] args) {
-        System.out.println( hasTeen(12,1,45));
+        System.out.println( hasTeen(1,1,45));
     }
     public static boolean hasTeen(int first, int second, int third){
         return (isTeen(first) || isTeen(second) || isTeen(third));
