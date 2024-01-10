3fe6e5dacb6fc62626430d344982c92a04c30456
manojleolappy <manojr1r@gmail.com>
2024-01-10 18:52:33 +0530

    UserInputForLoop&WhileLoop

---
+++ src/UserInput/Bascis.java
@@ -0,0 +1,13 @@
+package UserInput;
+
+import java.util.Scanner;
+
+public class Bascis {
+    public static void main(String[] args) {
+        System.console().readLine();
+        //or
+        Scanner scanner = new Scanner(System.in);
+//        scanner.nextLine(); scanner.nextDouble();scanner.next();
+
+    }
+}
