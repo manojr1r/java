fa7ee05861cb4085c9189c28ed267bbca3373846
manoj <manojabeta@gmail.com>
2024-01-22 22:10:41 +0530

    ReadingInput from SystemConsole and Scanner

--- src/UserInput/ScannerClass.java
+++ src/UserInput/ScannerClass.java
@@ -1,8 +1,15 @@
 package UserInput;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ScannerClass {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        try {
+            Scanner scannerFile = new Scanner(new File("ExcelSheet.xlsx"));
+        } catch (FileNotFoundException e) {
+            System.out.print("File Not Found");
+        }
 //        scanner.nextLine(); scanner.nextDouble();scanner.next();
     }
 }
--- src/UserInput/SystemConsole.java
+++ src/UserInput/SystemConsole.java
@@ -1,12 +1,24 @@
 package UserInput;
+import java.time.Year;
 
 public class SystemConsole {
     public static void main(String[] args) {
 //        All inputs are in string format then need to change to required format
+        System.out.println(getInputfromScanner());
+    }
+    public static <e> String getInputfromScanner(){
         String name = System.console().readLine("Hi What's your name? ");
         String year = System.console().readLine("What's your date of birth ");
-
-
-
+        int age;
+        while (true) {
+            try {
+                Integer.parseInt(year);
+                break;
+            } catch (Exception notAnIneger) {
+                year = System.console().readLine("Please enter Valid date of birth ");
+            }
+        }
+        age = Year.now().getValue() - Integer.parseInt(year);
+        return "Age "+ age + " Year's, Not just a number!!";
     }
 }
