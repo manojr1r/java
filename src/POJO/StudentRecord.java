3e40fa614340be3b4ac25aa33b576517aa4d36bd
manoj <manojabeta@gmail.com>
2024-01-25 01:18:35 +0530

    OOP1 part1

--- src/POJO/StudentRecord.java
+++ src/POJO/StudentRecord.java
@@ -4,8 +4,7 @@ public record StudentRecord(String id, String name, String dateofBirth, String c
     // It will create fields with same name as above
         /* all fields are "private" and "final" declared type "record" */
     /* Will create "constructor"
-    * and "getters" (but no special prefix like "get" or "is")
-    * can use direct field name like, "s.id()"
+    * and "getters" (but no special prefix like "get" or "is") can use direct field name like, "s.name()"
     */
     // will create "toString"
 // "NoSETTER" as all fields are final
