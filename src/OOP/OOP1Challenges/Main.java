0b8319ee0692a2eaaa342410cd30d31f25a5271e
manoj <manojabeta@gmail.com>
2024-01-26 19:19:17 +0530

    reached Inheritance

--- src/OOP/OOP1Challenges/Main.java
+++ src/OOP/OOP1Challenges/Main.java
@@ -3,6 +3,8 @@ package OOP.OOP1Challenges;
 public class Main {
     public static void main(String[] args) {
         Wall wall = new Wall(5,4);
+
+
         System.out.println("area= " + wall.getArea());
     }
 }
